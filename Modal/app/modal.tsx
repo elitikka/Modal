9b271bc430fb88ded1c7921b4190ef663e9b0376
elitikka <elin.tikkanen@gmail.com>
2025-11-12 12:54:12 +0200

    testing pressable component

--- Modal/app/modal.tsx
+++ Modal/app/modal.tsx
@@ -1,29 +1,30 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import {Pressable, StyleSheet, Text} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-
-export default function ModalScreen() {
+const App = () => {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Pressable
+          onPress={() => console.log('Pressed')}>
+            <Text style={styles.text}>Show modal message</Text>
+          </Pressable>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
+const styles=StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    justifyContent: 'center'
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+  text: {
+    fontSize: 18,
+    color: 'black'
+  }
+})
+
+export default App;
