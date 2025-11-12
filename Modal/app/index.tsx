5d47348ea4ca0af86757306442cc08258d1350fd
elitikka <elin.tikkanen@gmail.com>
2025-11-12 14:10:02 +0200

    modal toimii

--- Modal/app/index.tsx
+++ Modal/app/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text} from 'react-native';
+import { useState } from 'react';
+import {Modal, View, Pressable, StyleSheet, Text} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
-import { PressableComponent } from '@/components/presscomponent'; // show modal message -pressable component tehty erikseen komponentit -kansioon
 
 
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <PressableComponent/>
+        <Pressable onPress={() => setModalVisible(true)}>
+          <Text style={styles.text}>Show modal message</Text>
+        </Pressable>
+
+        <Modal
+          animationType='slide'
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)} // androidin oma takaisin-nappi?
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.text}>This is modal...</Text>
+
+                <Pressable onPress={() => setModalVisible(false)}>
+                  <Text style={styles.close}>Close</Text>
+                </Pressable>
+              </View>
+            </View>
+        </Modal>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -18,11 +39,36 @@ const styles=StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   text: {
     fontSize: 18,
-    color: 'black'
+    color: 'black',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    width: '100%',
+    margin: 20,
+    backgroundColor: '#e6e6e6ff',
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  close: {
+    marginTop: 30,
+    fontSize: 18,
+    fontWeight: 'bold',
   }
 })
 
